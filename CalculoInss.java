877e66e338112a5c1f850124c4ce76ff3e0096b0
Mariana Moreira <95227387+marisbm@users.noreply.github.com>
2021-11-29 11:42:37 -0300

    Add files via upload

---
+++ CalculoInss.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class CalculoInss {
+    public static void main(String[] args){
+        Scanner en = new Scanner(System.in);
+        System.out.print("Digite o valor do seu salário em R$:" );
+        double salario = en.nextDouble();
+        double imposto=0;
+
+        if(salario <=1100)
+            imposto = 0.075*salario;
+
+       else if(salario <=2203.48)
+            imposto = 0.09*salario;
+
+       else if(salario <=3305.22)
+            imposto = 0.12*salario;
+
+       else if(salario <=6433.57)
+            imposto = 0.14*salario;
+
+       else if(salario >6433.57)
+            imposto = 0.14*6433.57;
+
+       System.out.format("Seu imposto do inss é R$ %.2f",imposto);
+       en.close();
+
+    }
+
+}
