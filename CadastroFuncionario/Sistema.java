64848a31503d4f7be286cfe231f95e2e607c1430
Mariana Moreira <95227387+marisbm@users.noreply.github.com>
2021-12-01 11:37:03 -0300

    Update Sistema.java

--- CadastroFuncionario/Sistema.java
+++ CadastroFuncionario/Sistema.java
@@ -1,3 +1,4 @@
+
 package CadastroFuncionario;
 
 import java.text.ParseException;
@@ -9,15 +10,15 @@ public class Sistema {
     public static void main(String[] args) throws ParseException {
         ArrayList<Funcionario> funcionarios = new ArrayList<>();
         Scanner entrada = new Scanner(System.in);
-        double somaSalario = 0;
+        double somaSalario;
+        int codFuncionario;
 
-        do {
+        do{
             System.out.println("-------NOVO FUNCIONARIO----------");
-
-            funcionarios.add(new Funcionario());
             System.out.println("Digite o Codigo do funcionario: ");
-            funcionarios.get(funcionarios.size()-1).setCodFuncionario(entrada.nextInt());
-
+            codFuncionario = entrada.nextInt();
+                if(codFuncionario==0) break;
+            funcionarios.add(new Funcionario(codFuncionario));
             System.out.println("Digite o Nome do funcionario: ");
             funcionarios.get(funcionarios.size()-1).setNome(entrada.next());
 
@@ -27,18 +28,20 @@ public class Sistema {
             System.out.println("Digite a Data de admissão do funcionario dd/mm/aaaa: ");
             funcionarios.get(funcionarios.size()-1).setDataAdimissao(entrada.next());
 
-
-        }while (funcionarios.get(funcionarios.size()-1).getCodFuncionario()!=0);
+        } while(codFuncionario!=0);
 
         System.out.println("-----------INFORMAÇÕES---------------");
         System.out.println("Total de funcionarios na empresa: "+funcionarios.size());
+
+        if(!funcionarios.isEmpty()){
+
         System.out.println("Lista de funcionarios e o tempo de empresa: "+ funcionarios);
         System.out.println("------------------");
 
         somaSalario = funcionarios.stream().mapToDouble(funcionario -> Double.parseDouble(funcionario.getValorSalario())).sum();
 
-        System.out.println("Soma Salarial dos funcionarios da empresa: " + somaSalario);
-        System.out.println("Média Salarial dos funcionarios da empresa: " + somaSalario/funcionarios.size());
+        System.out.printf("Soma Salarial dos funcionarios da empresa: R$ %.2f\n", somaSalario);
+        System.out.printf("Média Salarial dos funcionarios da empresa: R$ %.2f\n", somaSalario/funcionarios.size());
 
         Comparator<Funcionario> comparadorSalario = (funcionario1, funcionario2) -> {
             if(Double.parseDouble(funcionario1.getValorSalario()) >= Double.parseDouble(funcionario2.getValorSalario())) return 1;
@@ -46,8 +49,13 @@ public class Sistema {
             return 0;
         };
 
-        System.out.println("Dados do Maior Salário: " + funcionarios.stream().max(comparadorSalario).get());
-        System.out.println("Dados do Menor Salário: "+  funcionarios.stream().min(comparadorSalario).get());
+        System.out.println("------------------");
+        System.out.println("Dados do Funcionario de Maior Salário: " + funcionarios.stream().max(comparadorSalario).get());
+        System.out.println("------------------");
+        System.out.println("Dados do Funcionario de Menor Salário: "+  funcionarios.stream().min(comparadorSalario).get());
+        }
         entrada.close();
+
+
     }
 }
